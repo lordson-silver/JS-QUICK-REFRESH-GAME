5b1ab0f54d27c5ee277604d1a7b060e204948fc6
lordson-silver <lordsonochuko5@gmail.com>
2022-07-19 13:14:21 +0100

    init refactor

--- script.js
+++ script.js
@@ -5,51 +5,76 @@ const feedback = document.getElementById("feedback");
 const randomNumEl = document.getElementById("randomNumber");
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
+
 let attempt = 3;
+let randomNum = () => {
+  return Math.floor(Math.random() * 20) + 1
+};
 
-randomNumEl.innerText = "?";
+const start = () => {
+  randomNumEl.innerText = "?";
+  input.value = "";
+  attempt = 3;
+  feedback.innerText = "FEEDBACK";
+  attemptEl.innerText = attempt;
+  console.log(randomNum());
+};
 
-let randomNum = Math.floor(Math.random() * 20) + 1;
+const hide = () => {
+  input.classList.toggle("hidden");
+};
 
-const gameOver = () => {
+function gameOver() {
   if (attempt <= 0) {
     feedback.innerText = "Game Over";
     submit.value = "Restart";
-    // input.setAttribute("class", "hidden");
-    input.classList.toggle("hidden"); // perfect
-  }
-};
+    hide(); // hide input
+  };
+}; 
 
 const restart = () => {
-  randomNum = Math.floor(Math.random() * 20) + 1;
-  console.log(randomNum);
   submit.value = "submit";
-  randomNumEl.innerText = "?";
-  attempt = 3;
-  input.classList.toggle("hidden");
-  input.value = "";
-  feedback.innerText = "FEEDBACK";
-  attemptEl.innerText = attempt;
+  start();
 };
+
+start();
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   if (submit.value === "submit") {
     let guess = Number(input.value);
-    if (guess === randomNum) {
-      feedback.innerText = "You Win";
-      submit.value = "Restart";
-      input.classList.toggle("hidden");
-      randomNumEl.innerText = String(guess);
-    } else if (guess < randomNum) {
-      feedback.innerText = "Too Low";
-      attempt--;
-    } else {
-      feedback.innerText = "Too High";
-      attempt--;
-    }
-    attemptEl.innerText = attempt; // NICE
-    gameOver();
+    // if (guess === randomNum) {
+    //   feedback.innerText = "You Win";
+    //   submit.value = "Restart";
+    //   hide(); // hide input
+    //   randomNumEl.innerText = String(guess);
+    // } else if (guess < randomNum) {
+    //   feedback.innerText = "Too Low";
+    //   attempt--;
+    // } else {
+    //   feedback.innerText = "Too High";
+    //   attempt--;
+    // }
+
+    // use switch statement to handle feedback
+    switch (true) {
+      case guess === randomNum():
+        feedback.innerText = "You Win";
+        submit.value = "Restart";
+        randomNumEl.innerText = String(guess);
+        break;
+      case guess < randomNum():
+        feedback.innerText = "Too Low";
+        attempt--;
+        break;
+      case guess > randomNum():
+        feedback.innerText = "Too High";
+        attempt--;
+        break;
+      default:  ;
+    };
+
+    attemptEl.innerText = attempt; // update attempts
   } else {
     restart();
   }
